4bfd3660c71b68ef189b97e7b1e42a930174cd49
dev-imran1 <freealltime247@gmail.com>
2024-08-01 03:29:17 +0600

    responsive

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// let slicderCounter = 0;
+// let slicderCounte = [
+//     "Developer",
+//     "Designer",
+//     "Youtube",
+//     "Code"
+// ];
+
+// let sliderValue = document.querySelector("#SliderValue")
+// function slide() {
+//     if (sliderValue >= slicderCounte.length) {
+//         slicderCounter = 0
+//     }
+//     sliderValue.innerHTML = "";
+//     sliderValue.classList.remove("holder_animation")
+//     void sliderValue.offsetWidth;
+//     sliderValue.classList.add("holder_animation");
+//     for (i = 0; i < slicderCounte[slicderCounter.length]; i++) {
+//         let letterDiv = document.createElement("div");
+//         letterDiv.innerHTML = slicderCounte[slicderCounter][i]
+
+//         if (letterDiv.innerHTML = "") {
+//             letterDiv.innerHTML = "&nbsp;";
+//         }
+//             letterDiv.classList.add("start");
+//             letterDiv.classList.add("animation");
+//             letterDiv.style.animationDelay = 1 / 10 + "s";
+//             sliderValue.appendChild(letterDiv)
+//         }
+//         slicderCounter++
+//     }
+//     slide();
+//     // setInterval(slide, 1000);
+
+//     $('.menu-btn').click(function(){
+//         $('.menu').toggleClass("active");
+//         $('.menu-btn i').toggleClass("active");
+
+//     })
+
+
+
+
+
